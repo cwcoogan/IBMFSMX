5e5649c58b12bf3c5c60fedc8896bc072ed39267
Emily Placet <emilyplacet@Emilys-MacBook-Pro.local>
2021-06-23 14:39:07 -0400

    home page progress

--- src/components/InformationRow.js
+++ src/components/InformationRow.js
@@ -1,36 +1,156 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 
 const InformationRow = () => {
   return (
-    <div className='row-container home-row'>
-          <div className='image-container-home'>
-              <Link to='/profile'>
+    <div className='row-container fluid-container home-row'>
+          <Popup
+        trigger={<div className='image-container-home'>
         <div className='image monitor' />
-        <div className='title-home'>What are Cloud Paks</div>
-                  </Link>
-      </div>
-        <div className='image-container-home'>
-              <Link to = "/Learn">
+        <div className='homePageText'>What are Cloud Paks?</div>
+      </div>}
+        modal
+        nested
+      >
+        {close => (
+          <div className="modal" style={{position:"inherit", display:"inherit"}} >
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+            <div className="display-4 font-weight-bold text-center mb-3"> What are Cloud Paks?</div>
+            <div className="content font-weight-bold">
+            • Team created the environment<br>
+              </br>
+              <br />
+              • Team began installing cloud paks<br>
+              </br>
+              <br />
+            </div>
+            <div className="actions">
+              <button
+                className="button"
+                onClick={() => {
+                  console.log('modal closed ');
+                  close();
+                }}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        )}
+      </Popup>
+      <Popup
+        trigger={<div className='image-container-home'>
         <div className='image learn' />
-        <div className='title-home'>Cloud Pak Benefits</div>
-        
-                  </Link>
-      </div>
-          <div className='image-container-home'>
-              <Link to='/questionnaire'>
+        <div className='homePageText'>Cloud Paks Benefits</div>
+      </div>}
+        modal
+        nested
+      >
+        {close => (
+          <div className="modal" style={{position:"inherit", display:"inherit"}} >
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+            <div className="display-4 font-weight-bold text-center mb-3"> Cloud Paks Benefits</div>
+            <div className="content font-weight-bold">
+            • Team created the environment<br>
+              </br>
+              <br />
+              • Team began installing cloud paks<br>
+              </br>
+              <br />
+            </div>
+            <div className="actions">
+              <button
+                className="button"
+                onClick={() => {
+                  console.log('modal closed ');
+                  close();
+                }}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        )}
+      </Popup>
+      <Popup
+        trigger={<div className='image-container-home'>
         <div className='image improve' />
-        <div className='title-home'>Impacts of Cloud Paks</div>
-        
-                  </Link>
-      </div>
-      <div className='image-container-home'>
-              <Link to = "/Learn">
+        <div className='homePageText'>Impacts of Cloud Paks</div>
+      </div>}
+        modal
+        nested
+      >
+        {close => (
+          <div className="modal" style={{position:"inherit", display:"inherit"}} >
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+            <div className="display-4 font-weight-bold text-center mb-3"> Impacts of Cloud Paks</div>
+            <div className="content font-weight-bold">
+            • Team created the environment<br>
+              </br>
+              <br />
+              • Team began installing cloud paks<br>
+              </br>
+              <br />
+            </div>
+            <div className="actions">
+              <button
+                className="button"
+                onClick={() => {
+                  console.log('modal closed ');
+                  close();
+                }}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        )}
+      </Popup>
+      <Popup
+        trigger={<div className='image-container-home'>
         <div className='image learn2' />
-        <div className='title-home'>How Cloud Paks Help</div>
-        
-                  </Link>
-      </div>
+        <div className='homePageText'>How Cloud Paks Help</div>
+      </div>}
+        modal
+        nested
+      >
+        {close => (
+          <div className="modal" style={{position:"inherit", display:"inherit"}} >
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+            <div className="display-4 font-weight-bold text-center mb-3"> How Cloud Paks Help</div>
+            <div className="content font-weight-bold">
+            • Team created the environment<br>
+              </br>
+              <br />
+              • Team began installing cloud paks<br>
+              </br>
+              <br />
+            </div>
+            <div className="actions">
+              <button
+                className="button"
+                onClick={() => {
+                  console.log('modal closed ');
+                  close();
+                }}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        )}
+      </Popup>
+      
     </div>
   );
 };
