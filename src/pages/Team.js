6d0aaf40ba0893be1eeabbd3d960ae9bfdf149bc
Alejandro Obrador <alexobrador1619@gmail.com>
2021-06-23 11:25:34 -0400

    added new utility classes and start team layout

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -3,6 +3,8 @@ import Accordion from 'react-bootstrap/Accordion';
 import TeamInfoRow from '../components/TeamInfoRow';
 import Card from 'react-bootstrap/Card';
 import 'reactjs-popup/dist/index.css';
+import contactImgFir from '../images/ContactVisual.png';
+import teamImgCircle from '../images/pictureIconTeam.png';
 
 const Team = () => {
   return (
@@ -21,7 +23,58 @@ const Team = () => {
             </div>
           </div>
         </div>
-        <TeamInfoRow />
+        
+        <div className="container">
+        <div className='row mt-5 hero-row-reverse mobile home-row'>
+          <div className='col-lg-6 txt-md-left'>
+            <div className='row mb-3'></div>
+            <img
+              src={teamImgCircle}
+              className='team-image'
+              alt='Cicle Icons'
+            />
+            <p className='text-margin bold'>
+              Chase Coogan<br></br>
+              -
+            </p>
+          </div>    
+        </div>
+      </div>
+
+      <div className="container">
+        <div className='row mt-5 hero-row-reverse mobile home-row'>
+          <div className='col-lg-6 txt-md-left'>
+            <div className='row mb-3'></div>
+            <img
+              src={teamImgCircle}
+              className='team-image'
+              alt='Cicle Icons'
+            />
+            <p className='text-margin bold'>
+              John Lucas<br></br>
+              -
+            </p>
+          </div>    
+        </div>
+      </div>
+
+      <div className="container">
+        <div className='row mt-5 hero-row-reverse mobile home-row'>
+          <div className='col-lg-6 txt-md-left'>
+            <div className='row mb-3'></div>
+            <img
+              src={teamImgCircle}
+              className='team-image'
+              alt='Cicle Icons'
+            />
+            <p className='text-margin bold'>
+              Matt Konwiser<br></br>
+              -
+            </p>
+          </div>    
+        </div>
+      </div>
+
       </div>
       
     </div>
