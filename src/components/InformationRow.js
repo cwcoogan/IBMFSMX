c128eb4ad16e67ad1ee6605e5993019e10d0d794
Emily Placet <emilyplacet@Emilys-MacBook-Pro.local>
2021-06-23 10:36:39 -0400

    home page progess

--- src/components/InformationRow.js
+++ src/components/InformationRow.js
@@ -7,27 +7,27 @@ const InformationRow = () => {
           <div className='image-container-home'>
               <Link to='/profile'>
         <div className='image monitor' />
-        <div className='title-home'>Monitor</div>
+        <div className='homePageTitle'>Monitor</div>
                   </Link>
       </div>
         <div className='image-container-home'>
               <Link to = "/Learn">
         <div className='image learn' />
-        <div className='title-home'>Learn</div>
+        <div className='homePageTitle'>Learn</div>
         
                   </Link>
       </div>
           <div className='image-container-home'>
               <Link to='/questionnaire'>
         <div className='image improve' />
-        <div className='title-home'>Improve</div>
+        <div className='homePageTitle'>Improve</div>
         
                   </Link>
       </div>
       <div className='image-container-home'>
               <Link to = "/Learn">
         <div className='image learn2' />
-        <div className='title-home'>learn2</div>
+        <div className='homePageTitle'>learn2</div>
         
                   </Link>
       </div>
