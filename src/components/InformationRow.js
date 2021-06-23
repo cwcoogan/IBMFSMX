27f3136fc551ee888e45b242f35817b9c35da801
cwcoogan <chasecoogan12@gmail.com>
2021-06-23 10:55:39 -0400

    homepage update 4 boxes

--- src/components/InformationRow.js
+++ src/components/InformationRow.js
@@ -7,27 +7,27 @@ const InformationRow = () => {
           <div className='image-container-home'>
               <Link to='/profile'>
         <div className='image monitor' />
-        <div className='homePageTitle'>Monitor</div>
+        <div className='title-home'>Cloud Paks</div>
                   </Link>
       </div>
         <div className='image-container-home'>
               <Link to = "/Learn">
         <div className='image learn' />
-        <div className='homePageTitle'>Learn</div>
+        <div className='title-home'>Benefit</div>
         
                   </Link>
       </div>
           <div className='image-container-home'>
               <Link to='/questionnaire'>
         <div className='image improve' />
-        <div className='homePageTitle'>Improve</div>
+        <div className='title-home'>Impact</div>
         
                   </Link>
       </div>
       <div className='image-container-home'>
               <Link to = "/Learn">
         <div className='image learn2' />
-        <div className='homePageTitle'>learn2</div>
+        <div className='title-home'>How Cloud Paks Help</div>
         
                   </Link>
       </div>
