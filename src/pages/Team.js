044030490d0636bfa7375a09182f6ce07598e3a9
Alejandro Obrador <alexobrador1619@gmail.com>
2021-06-24 13:17:23 -0400

    added headers for teampage

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -14,7 +14,49 @@ const Team = () => {
           <div className='col-lg-12 txt-md-left'>
             <div className='row mb-3'>
               <h1 className='display-5 font-weight-bold'>
-                Team
+                Project Leads
+              </h1>
+              <h1 className='hr-purple'>
+                
+              </h1>
+  
+            </div>
+          </div>
+        </div>
+
+        <div className='row mt-5 hero-row-reverse-mobile home-row '>
+          <div className='col-lg-12 txt-md-left'>
+            <div className='row mb-3'>
+              <h1 className='display-5 font-weight-bold'>
+                Environment Configuration
+              </h1>
+              <h1 className='hr-purple'>
+                
+              </h1>
+  
+            </div>
+          </div>
+        </div>
+
+        <div className='row mt-5 hero-row-reverse-mobile home-row '>
+          <div className='col-lg-12 txt-md-left'>
+            <div className='row mb-3'>
+              <h1 className='display-5 font-weight-bold'>
+                Architect/Installers
+              </h1>
+              <h1 className='hr-purple'>
+                
+              </h1>
+  
+            </div>
+          </div>
+        </div>
+
+        <div className='row mt-5 hero-row-reverse-mobile home-row '>
+          <div className='col-lg-12 txt-md-left'>
+            <div className='row mb-3'>
+              <h1 className='display-5 font-weight-bold'>
+                Interns
               </h1>
               <h1 className='hr-purple'>
                 
@@ -37,6 +79,7 @@ const Team = () => {
               Chase Coogan<br></br>
               -
             </p>
+            
           </div>    
         </div>
       </div>
