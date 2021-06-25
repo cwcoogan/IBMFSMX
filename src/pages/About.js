0d7748c94e2654ab7152d44d4133628529b9a4d1
Eduardo Gomez <egomez8@binghamton.edu>
2021-06-25 16:17:27 -0400

    Edits on About Page

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,52 +11,38 @@ import 'reactjs-popup/dist/index.css';
 const Tips = () => {
   return (
     <div>
-      <div className='container'>
-        <div className='row mt-5 hero-row-reverse-mobile home-row '>
-          <div className='col-lg-12 txt-md-left'>
-            <div className='row mb-3'>
-              <h1 className='display-5 font-weight-bold'>
-              IBM FSMX Goals
-              </h1>
-              <h1 className='hr-purple'>
-            
-              </h1>
-            </div>
+
+  <div class="boxcontainer">
+    <div class="box">
+      <ul> 
+        <li>
+          <div class="box-content">
+            <h2 class="date">About</h2>
+            <p>An awesome webiste to show off environment </p>
           </div>
-        </div>
-      </div> 
-      
-      <div className="container">
-        <div className='row mt-5 hero-row-reverse mobile home-row'>
-          <div className='col-lg-6 txt-md-left'>
-            <div className='row mb-3'></div>
-            <p className='lead font-weight bold'>
-              <ul>
-                <li>To create a one stop, shop for sellers</li>
-                <li>Provide real use-case examples </li> 
-                <li>Provide the best solutions for our clients </li>
-                <li>Cross sell our products for ultimate solutions</li>
-              </ul>
-            </p>
-          </div> 
-          <div className='col-lg-5'>
-            <div className='hero-img-right'>
-              <img
-                src={aboutFirst}
-                className='fluid-img'
-                alt='Contact Page Image'
-              />  
-            </div>
-          </div> 
-        </div>
-      </div>
+        </li>
+
+      </ul>
+  </div>
+  <div class="box">
+      <ul> 
+        <li>
+          <div class="box-content">
+            <h2 class="date">Mission</h2>
+            <p>To sell a lot and make a lot of money </p>
+          </div>
+        </li>
+
+      </ul>
+  </div>
+</div>
 
       <div className='container'>
         <div className='row mt-5 hero-row-reverse-mobile home-row '>
           <div className='col-lg-12 txt-md-left'>
             <div className='row mb-3'>
               <h1 className='display-5 font-weight-bold'>
-              Experience Roadmap
+              Roadmap
               </h1>
               <h1 className='hr-purple'>
             
