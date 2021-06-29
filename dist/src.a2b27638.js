f953ac8e80616c0ef8c6136c5b55c533c01fb2fc
Alejandro Obrador <alexobrador1619@gmail.com>
2021-06-29 09:10:47 -0400

    fix cache bug when pushing

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -86556,7 +86556,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50215" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50319" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
