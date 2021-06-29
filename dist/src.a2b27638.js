5cd2d2345cb4ca1bdfe28f4b8cdff8aa03a95b0c
Alejandro Obrador <alexobrador1619@gmail.com>
2021-06-29 08:52:30 -0400

    fix cache bug

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -86556,7 +86556,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50100" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50215" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
