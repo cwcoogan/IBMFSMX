1e88581de71ab8f05a02a72b62f95c1380843708
Eduardo Gomez <egomez8@binghamton.edu>
2021-07-09 18:13:09 -0400

    Progress on blog page

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -81,19 +81,9 @@ const Blog = () => {
       <div class="specinbox">  
         <ul> 
           <li>
-            <div class="specin-content">
-            
               <h2 class='alignleft'> Cool Blog One </h2>
               <h3 class='alignright'>Published: July 7th 2021</h3>
-              <h1> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna 
-                aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
-                ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit 
-                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint 
-                occaecat cupidatat non proident, sunt in culpa qui officia 
-                deserunt mollit anim id est laborum.</h1>
-            </div>
+              <h2> Short blog intro here</h2>
           </li>
 
         </ul>
