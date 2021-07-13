ef7dbcca9f49ca0c8846401033c524c3d0d7daba
Alejandro Obrador <alexobrador1619@gmail.com>
2021-07-13 16:22:21 -0400

    wip contact site, new accordion styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Accordion from 'react-bootstrap/Accordion';
+import ReactDOM from "react-dom";
+import Accordion from "../components/Accordion";
 import Card from 'react-bootstrap/Card';
 import 'reactjs-popup/dist/index.css';
 import contactImgFir from '../images/contact.png';
@@ -12,11 +13,13 @@ const Contact = () => {
           <div className='col-lg-12 txt-md-left'>
             <div className='row mb-3'>
               <h1 className='display-5 font-weight-bold'>
-              Contact
+              Contact us
               </h1>
               <h1 className='hr-purple'>
-            
               </h1>
+              
+            <h4><br></br>Looking for services to be added or have general questions? <br>
+            </br>Send us a email and we will be in touch. </h4>
             </div>
           </div>
         </div>
@@ -26,9 +29,9 @@ const Contact = () => {
         <div className='row mt-5 hero-row-reverse mobile home-row'>
           <div className='col-lg-6 txt-md-left'>
             <div className='row mb-3'></div>
-            <p className='lead font-weight-bold'>
+            <h4 className='font-weight-bold'>
               For questions email:
-            </p>
+            </h4>
             <p className='lead font-weight-bold'>
               ● Chase Coogan: chase.coogan@ibm.com<br></br>
               ● John Lucas: john.lucas@ibm.com
@@ -36,94 +39,51 @@ const Contact = () => {
           </div> 
           <div className='col-lg-5'>
             <div className='hero-img-right'>
-              <img
-                src={contactImgFir}
-                className='contact-page-img'
-                alt='Contact Page Image'
-              />
+              
             </div>
           </div> 
         </div>
       </div>
 
     
-      <div className='container'>
-        <h1 className='display-5 font-weight-bold'>FAQ</h1>
-        <h1 className='hr-purple'> </h1>
-      </div>
-      <div className='container display-5 mobile contact-row'>
-      <Accordion className='pt-0 pb-0'>
-        <Card className='accordion-section'>
-          <Accordion.Toggle as={Card.Header} eventKey='0'>
-            <h5>How do I access the environment?</h5>
-          </Accordion.Toggle>
-          <Accordion.Collapse eventKey='0' className='accordion-tips'>
-            <Card.Body>
-              <h6>IBM IBM IBM IBM IBM IBM</h6>
-            </Card.Body>
-          </Accordion.Collapse>
-          <Accordion.Collapse eventKey='0' className='accordion-tips'>
-            <Card.Body>
-              <h6>
-                remove this line if you do not want to seperate paragraphs
-              </h6>
-            </Card.Body>
-          </Accordion.Collapse>
-		  <Accordion.Collapse eventKey='0' className='accordion-tips'>
-            <Card.Body>
-              <h6>
-                remove this line if you do not want to seperate paragraphs
-              </h6>
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-        <Card className='accordion-section'>
-          <Accordion.Toggle as={Card.Header} eventKey='1'>
-            <h5>What is a Cloud Pak?</h5>
-          </Accordion.Toggle>
-          <Accordion.Collapse eventKey='1' className='accordion-tips'>
-            <Card.Body>
-              <h6>IBM IBM IBM IBM</h6>
-            </Card.Body>
-          </Accordion.Collapse>
-          <Accordion.Collapse eventKey='1' className='accordion-tips'>
-            <Card.Body>
-              <h6>remove this line if you do not want to seperate paragraphs</h6>
-            </Card.Body>
-          </Accordion.Collapse>
-          <Accordion.Collapse eventKey='1' className='accordion-tips'>
-            <Card.Body>
-              <h6>remove this line if you do not want to seperate paragraphs</h6>
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-        <Card className='accordion-section'>
-          <Accordion.Toggle as={Card.Header} eventKey='2'>
-            <h5>Where can I learn more?</h5>
-          </Accordion.Toggle>
-          <Accordion.Collapse eventKey='2' className='accordion-tips'>
-            <Card.Body>
-              <h6>IBM IBM IBM IBM IBM</h6>
-            </Card.Body>
-          </Accordion.Collapse>
-          <Accordion.Collapse eventKey='2' className='accordion-tips'>
-            <Card.Body>
-              <h6>remove this line if you do not want to seperate paragraphs</h6>
-            </Card.Body>
-          </Accordion.Collapse>
-		  <Accordion.Collapse eventKey='2' className='accordion-tips'>
-            <Card.Body>
-              <h6>remove this line if you do not want to seperate paragraphs</h6>
-            </Card.Body>
-          </Accordion.Collapse>
-        </Card>
-       
-      </Accordion>
+      
+      <div className='container mobile home-row'>
+      <Accordion className='accordion'
+        title="How do I access the environment"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Accordion className='accordion'
+        title="What is a Cloud Pak"
+        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      />
+      <Accordion className='accordion'
+        title="Where can I learn more"
+        content="
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        "
+      />
+      <Accordion className='accordion'
+        title="How do I add a service"
+        content="
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        "
+      />
+      
+    
     </div>  
     </div>
 
         
   );
 };
+
 
 export default Contact;
