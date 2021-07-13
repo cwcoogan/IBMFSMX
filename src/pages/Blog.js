583dd813cd680fe3bfdba382804a629a895c59f3
Eduardo Gomez <egomez8@binghamton.edu>
2021-07-13 16:59:45 -0400

    Added content to blog and edited format

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BloImgFir from '../images/Blog1.png';
 import BloImgSec from '../images/Blog2.png';
 import BloImgThi from '../images/Blog3.png';
+import BloDocFir from '../images/CloudPakOne.pdf';
 import 'reactjs-popup/dist/index.css';
 import Popup from 'reactjs-popup';
 
@@ -13,7 +14,7 @@ const Blog = () => {
             <div className='col-lg-12 txt-md-left'>
               <div className='row mb-3'>
                 <h1 className='display-5 font-weight-bold'>
-                  Blog & Use Cases
+                  Blog & White Papers
                 </h1>
                 <h1 className='hr-purple'>
                 </h1>
@@ -34,7 +35,7 @@ const Blog = () => {
                 src={BloImgFir}
               />
           </div>
-            <h2 class="date">Banking</h2>
+            <h2 class="date">General</h2>
           </div>
         </li>
 
@@ -49,7 +50,7 @@ const Blog = () => {
                 src={BloImgSec}
               />
           </div>
-            <h2 class="date">Insurance</h2>
+            <h2 class="date">Banking</h2>
           </div>
         </li>
 
@@ -64,7 +65,7 @@ const Blog = () => {
                 src={BloImgThi}
               />
           </div>
-            <h2 class="date">Misc</h2>
+            <h2 class="date">Insurance</h2>
           </div>
         </li>
 
@@ -74,13 +75,171 @@ const Blog = () => {
 
     <div class = 'container'>
     <div class = 'industrynames'>
-        <h1>Banking</h1>
+        <h1>General</h1>
         <h1 className='hr-purple'></h1>
       </div>
 
     
     <div class="specincontainer">
 
+    <Popup
+        trigger={<div className='specinbox'>
+        <div className='specin-content' />
+        <div class ='specin-content'>
+        <ul> 
+          <li>
+              <h1> Tech Tuesday Kick-Off!</h1>
+              <h2> Published: 7/13/2021</h2>
+              <h3> Check out the very first of many blogs & 
+                hear from our own Technical Leader...
+              </h3>
+          </li>
+        </ul>
+        </div>
+      </div>}
+        modal
+        nested
+      >
+        {close => (
+          <div className="modal" style={{position:"inherit", display:"inherit"}} >
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+            
+            <div className="header"> Tech Tuesday Kick-Off </div>
+            <div className="content">
+            
+            Team - <br></br>
+            <br></br>
+            Welcome to the second half, I hope you are all rejuvenated from the Holiday 
+            break and are ready for another great quarter. My name is Chase Coogan, and 
+            I am a Cross Brand Cloud Pak Technical Leader in the Financial Market. My 
+            team and I are in charge of leading the project to bring to life a 
+            multi-cluster environment consisting of several Cloud Paks to show the 
+            ease of integration and to give a tangible asset to our technical team for 
+            customer demonstrations. I am writing to you to shed light on the business 
+            value Cloud Paks have on our customers, so please join me on this journey, 
+            and welcome to Tech Tuesdays! <br></br>
+            <br></br>
+            Through Tech Tuesdays, I aim to instill the business value in each of you 
+            behind moving to Cloud Paks, as well as to provide customer feedback, and 
+            share success stories. Of course, we care about the quantified data, so I 
+            will be sure to back my statements up with factual statistics. As Cloud Paks
+            continue to evolve, we need to begin having the conversation with clients 
+            around if moving to a Cloud Pak is right for them. <br></br>
+            <br></br>
+            One of the questions that I receive more often than not is why Cloud Paks? 
+            And the answer I give is simple, Cloud Paks are an easy to deploy solution, 
+            that will allow our customers to easily manage their applications and 
+            accelerate development by up to 66%. Sounds like a bunch of buzzwords, 
+            doesn’t it? Well, it’s true, the underlying technology gives Developers and 
+            their DevOps team more freedom to test new code and features to push to a 
+            final application. <br></br>
+            <br></br>            
+            Afterall, IBM is named a Leader in the 2021 Gartner Magic Quadrant for Cloud 
+            AI Developer Services, and a leader in the Forrester Wave: Multicloud 
+            Container Development Platforms in 2020. In the 2019 Total Economic Report, 
+            Forrester identified the overall cost savings associated with moving to a 
+            Cloud Pak, and some of the numbers aren’t so surprising. <br></br>
+            <br></br>
+            Forrester found savings amongst the infrastructure layer by reducing hardware, 
+            cloud and licensing costs. In fact, the total cost of ownership by migrating 
+            to a Cloud Pak was reduced by 4%, hardware costs decreased by 44%, and 
+            licensing costs went down by 50%. The numbers are staggering, but that is the 
+            benefit in moving to a Cloud Pak. <br></br>
+            <br></br>
+            In summary, customers business models can be complex and implementing a solution 
+            to deal with their end-to-end needs can be costly. That is why migrating to a 
+            Cloud Pak is an efficient way to scale their business while reducing their TCO. 
+            The next time you speak with your clients, be sure to drill in on some of the 
+            factual data available to help boost your chance of making it a Cloud Pak deal! <br></br>
+            <br></br> 
+            Thank you for joining me on the kick-off of Tech Tuesdays, and if you have any 
+            questions do not hesitate to contact me via slack, email, or phone. <br></br>
+            <br></br>
+            Best, <br></br>
+            Chase Coogan
+
+
+              
+            </div>
+            <div className="actions">
+              <button
+                className="popup-button"
+                onClick={() => {
+                  console.log('modal closed ');
+                  close();
+                }}
+              >
+                Close
+              </button>
+            </div>
+            
+          </div>
+        )}
+      </Popup>
+
+      <Popup
+        trigger={<div className='specinbox'>
+        <div className='specin-content' />
+        <div class ='specin-content'>
+        <ul> 
+          <li>
+              <h1> Cloud Pak One Pager</h1>
+              <h2> Published: 7/13/2021</h2>
+              <h3> Interested in learning more about Cloud Paks? Check out our one pager...
+              </h3>
+          </li>
+        </ul>
+        </div>
+      </div>}
+        modal
+        nested
+      >
+        {close => (
+          <div className="modal" style={{position:"inherit", display:"inherit"}} >
+            <button className="close" onClick={close}>
+              &times;
+            </button>
+            
+            <div className="header"> Cloud Pak One Pager </div>
+            <div className="content">
+            
+            <iframe src={BloDocFir} height='400px' width='600px'>
+            </iframe> 
+              
+              <br></br>
+              
+            </div>
+            <div className="actions">
+                
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+              <button
+                className="popup-button"
+                onClick={() => {
+                  console.log('modal closed ');
+                  close();
+                }}
+              >
+                Close
+              </button>
+            </div>
+            
+          </div>
+        )}
+      </Popup>
+
+    </div>
+    </div>
+
+    <div class = 'container'>
+    <div class = 'industrynames'>
+        <h1>Banking</h1>
+        <h1 className='hr-purple'></h1>
+      </div>
+
+    <div class="specincontainer">
     <Popup
         trigger={<div className='specinbox'>
         <div className='specin-content' />
@@ -116,15 +275,6 @@ const Blog = () => {
               
             </div>
             <div className="actions">
-              <button
-                className="popup-button"
-                onClick={() => {
-                  console.log('modal transferred to contact ');
-                  window.location.href='/contact';
-                }}
-              >
-                Contact Us 
-              </button>
                 
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
@@ -178,15 +328,6 @@ const Blog = () => {
               
             </div>
             <div className="actions">
-              <button
-                className="popup-button"
-                onClick={() => {
-                  console.log('modal transferred to contact ');
-                  window.location.href='/contact';
-                }}
-              >
-                Contact Us 
-              </button>
                 
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
@@ -204,7 +345,7 @@ const Blog = () => {
           </div>
         )}
       </Popup>
-
+      
     </div>
     </div>
 
@@ -250,15 +391,6 @@ const Blog = () => {
               
             </div>
             <div className="actions">
-              <button
-                className="popup-button"
-                onClick={() => {
-                  console.log('modal transferred to contact ');
-                  window.location.href='/contact';
-                }}
-              >
-                Contact Us 
-              </button>
                 
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
@@ -312,149 +444,6 @@ const Blog = () => {
               
             </div>
             <div className="actions">
-              <button
-                className="popup-button"
-                onClick={() => {
-                  console.log('modal transferred to contact ');
-                  window.location.href='/contact';
-                }}
-              >
-                Contact Us 
-              </button>
-                
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-              <button
-                className="popup-button"
-                onClick={() => {
-                  console.log('modal closed ');
-                  close();
-                }}
-              >
-                Close
-              </button>
-            </div>
-            
-          </div>
-        )}
-      </Popup>
-      
-    </div>
-    </div>
-
-    <div class = 'container'>
-    <div class = 'industrynames'>
-        <h1>Misc</h1>
-        <h1 className='hr-purple'></h1>
-      </div>
-
-    <div class="specincontainer">
-    <Popup
-        trigger={<div className='specinbox'>
-        <div className='specin-content' />
-        <div class ='specin-content'>
-        <ul> 
-          <li>
-              <h1> Example Blog Title</h1>
-              <h2> Published July 12th 2021</h2>
-              <h3> This will be a short intro to the blog with '...' so people
-                can click on it and read more
-              </h3>
-          </li>
-        </ul>
-        </div>
-      </div>}
-        modal
-        nested
-      >
-        {close => (
-          <div className="modal" style={{position:"inherit", display:"inherit"}} >
-            <button className="close" onClick={close}>
-              &times;
-            </button>
-            
-            <div className="header"> Example Blog Title </div>
-            <div className="content">
-            
-            Example Blog Title<br></br>
-           
-           Looking for services to be added?
-              
-              <br></br>
-              
-            </div>
-            <div className="actions">
-              <button
-                className="popup-button"
-                onClick={() => {
-                  console.log('modal transferred to contact ');
-                  window.location.href='/contact';
-                }}
-              >
-                Contact Us 
-              </button>
-                
-              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-
-              <button
-                className="popup-button"
-                onClick={() => {
-                  console.log('modal closed ');
-                  close();
-                }}
-              >
-                Close
-              </button>
-            </div>
-            
-          </div>
-        )}
-      </Popup>
-
-      <Popup
-        trigger={<div className='specinbox'>
-        <div className='specin-content' />
-        <div class ='specin-content'>
-        <ul> 
-          <li>
-              <h1> Example Blog Title</h1>
-              <h2> Published July 12th 2021</h2>
-              <h3> This will be a short intro to the blog with '...' so people
-                can click on it and read more
-              </h3>
-          </li>
-        </ul>
-        </div>
-      </div>}
-        modal
-        nested
-      >
-        {close => (
-          <div className="modal" style={{position:"inherit", display:"inherit"}} >
-            <button className="close" onClick={close}>
-              &times;
-            </button>
-            
-            <div className="header"> Example Blog Title </div>
-            <div className="content">
-            
-            Example Blog Title<br></br>
-           
-           Looking for services to be added?
-              
-              <br></br>
-              
-            </div>
-            <div className="actions">
-              <button
-                className="popup-button"
-                onClick={() => {
-                  console.log('modal transferred to contact ');
-                  window.location.href='/contact';
-                }}
-              >
-                Contact Us 
-              </button>
                 
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
