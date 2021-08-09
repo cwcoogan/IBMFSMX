5567a38b42aa7f436690c9a3beeb703550ae5c4d
Alejandro Obrador <alexobrador1619@gmail.com>
2021-08-09 15:19:38 -0400

    workflow changes

--- src/pages/Workflow.js
+++ src/pages/Workflow.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import ClustersInformationRow from '../components/ClustersInformationRow';
-import ClustersInformationSecondRow from '../components/ClustersInformationSecondRow';
-import AppliancesInformationRow from '../components/AppliancesInformationRow';
 import 'reactjs-popup/dist/index.css';
-import Arch from '../images/Arch.png';
 
-const Installations = () => {
+const Workflow = () => {
   return (
       <div>
       
@@ -25,30 +21,51 @@ const Installations = () => {
         </div>
       </div> 
 
-      <ClustersInformationRow />
-      <ClustersInformationSecondRow />
+      <div class="tree">
+	      <ul>
+		      <li>
+			      <a href="#">Parent</a>
+			      <ul>
+				      <li>
+					<a href="#">Child</a>
+					<ul>
+						<li>
+							<a href="#">Grand Child</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<a href="#">Child</a>
+					<ul>
+						<li><a href="#">Grand Child</a></li>
+						<li>
+							<a href="#">Grand Child</a>
+							<ul>
+								<li>
+									<a href="#">Great Grand Child</a>
+								</li>
+								<li>
+									<a href="#">Great Grand Child</a>
+								</li>
+								<li>
+									<a href="#">Great Grand Child</a>
+								</li>
+							</ul>
+						</li>
+						<li><a href="#">Grand Child</a></li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+	</ul>
+</div>
 
-      {/* appliances */}
-      <div className='container'>
-        <div className='row mt-5 hero-row-reverse-mobile'>
-          <div className='col-lg-12 txt-md-left'>
-            <div className='row mb-3'>
-              <h1 className='display-5 font-weight-bold'>
-              Appliances
-              </h1>
-              <h1 className='hr-purple'>
-            
-              </h1>
-            </div>
-          </div>
-        </div>
-      </div> 
-
-      <AppliancesInformationRow />
       
     </div>
+
+    
 
   );
 };
 
-export default Installations;
+export default Workflow;
