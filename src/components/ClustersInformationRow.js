49537c8cb1bdcb58d9a76b265f0f847f01a878ff
Alejandro Obrador <alexobrador1619@gmail.com>
2021-08-13 11:44:58 -0400

    clusters popups styling

--- src/components/ClustersInformationRow.js
+++ src/components/ClustersInformationRow.js
@@ -5,6 +5,7 @@ import 'reactjs-popup/dist/index.css';
 import { Switch, Route, Link } from 'react-router-dom';
 import Contact from '../pages/Contact';
 import rajesh from "../images/profile-pic/rajesh-kartha.png";
+import jerry from "../images/profile-pic/jerry-steward.png"
 
 const ClustersInformationRow = () => {
   return (
@@ -34,11 +35,11 @@ const ClustersInformationRow = () => {
           <div className='col-lg-6 txt-md-left'>
   
             <h3>[Installer]</h3>
-             <h4>&nbsp;&nbsp;Rajesh Kartha</h4>
-             <b>&nbsp;&nbsp;rajesh.kartha@ibm.com</b>
+             <h4>Rajesh Kartha</h4>
+             <b>kartha@us.ibm.com</b>
           </div>
           <div className=''>
-            <div className='hero-img-center'>
+            <div className='hero-img-right'>
             <img
                 src={rajesh}
                 className='contact-page-img'
@@ -119,22 +120,45 @@ const ClustersInformationRow = () => {
             <div className="header modalText2"> Cloud Pak for Integration </div>
             
             <div className="content modalText">
-             Cloud Pak 4 Integration is running on OpenShift 4.6 <br>
-            </br><br></br>
+            <div className='row mt-5 hero-row-reverse-mobile'>
+          <div className='col-lg-6 txt-md-left'>
+  
+            <h3>[Installer]</h3>
+             <h4>Eric Lowry</h4>
+             <b>elowry@us.ibm.com</b>
+          </div>
+          <div className=''>
+            <div className='hero-img-right'>
+            <img
+                src={rajesh}
+                className='contact-page-img'
+                alt='Sitting on questionnaire'
+              />
+            </div>
+          </div>
+          </div>
+
+          <div className="hr-black"></div>
+            {/* <div className="content font-weight-bold"> */}
+               
+              <h3>[Installed Services]</h3>
+             
+                &nbsp;● API Connect<br></br>
+                &nbsp;● Data Power Gateaway<br></br>
+                <div className="hr-black"></div>  
+                <h3>[Versions]</h3>
+             
+                &nbsp;● Lopsum ipsum <br></br>
+                &nbsp;● Lopsum ipsum <br></br>
+                
+                <div className="hr-black"></div>
+                <h4>Cloud Pak 4 Integration is running on OpenShift 4.6</h4> 
+             <br></br>
               [ABOUT]
               <br /><br></br>
-              Installer: 
-              <h1 className='hr-purple2'></h1> 
-              Eric Lowery<br></br><br></br>
-              {/* <br /> */}
-              Installed Services:<br></br>
-              <h1 className='hr-purple2'></h1> 
-                API Connect <br></br>
-                Data Power Gateway<br></br><br></br>
-                
-                Looking for services to be added?
+              <div className="hr-black"></div>
+              <center>Looking for services to be added?</center>
               
-              <br></br>
            
             </div>
             <div className="actions">
--- src/components/ClustersInformationSecondRow.js
+++ src/components/ClustersInformationSecondRow.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
+import jonathanT from "../images/profile-pic/jonathan-tomasulo.png"
+import douglas from "../images/profile-pic/douglas-freeby.png"
+import robert from "../images/profile-pic/robert-pugh.png"
 
 const ClustersInformationSecondRow = () => {
   return (
@@ -24,26 +27,50 @@ const ClustersInformationSecondRow = () => {
             <div className="header modalText2"> Cloud Pak for Security </div>
             
             <div className="content modalText">
-             Cloud Pak 4 Security is running on OpenShift 4.6 <br>
-            </br><br></br>
+            <div className='row mt-5 hero-row-reverse-mobile'>
+          <div className='col-lg-6 txt-md-left'>
+  
+            <h3>[Installer]</h3>
+             <h4>Jonathan Tomasulo</h4>
+             <b>jptomasu@us.ibm.com</b>
+          </div>
+          <div className=''>
+            <div className='hero-img-right'>
+            <img
+                src={jonathanT}
+                className='contact-page-img'
+                alt='Sitting on questionnaire'
+              />
+            </div>
+          </div>
+          </div>
+
+          <div className="hr-black"></div>
+            {/* <div className="content font-weight-bold"> */}
+               
+              <h3>[Installed Services]</h3>
+             
+                &nbsp;● Threat Intelligence Insights <br></br>
+                &nbsp;● Data Explorer<br></br>
+                &nbsp;● SOAR<br></br>
+                &nbsp;● Risk Manager<br></br>
+                &nbsp;● Guardium Insights S-Tap<br></br>
+                <div className="hr-black"></div>  
+                <h3>[Versions]</h3>
+             
+                &nbsp;● Lopsum ipsum <br></br>
+                &nbsp;● Lopsum ipsum <br></br>
+                
+                <div className="hr-black"></div>
+                <h4>Cloud Pak 4 Security is running on OpenShift 4.6</h4> 
+             <br></br>
               [ABOUT]
               <br /><br></br>
-              Installer: 
-              <h1 className='hr-purple2'></h1> 
-              Jonathan Tomasulo<br></br><br></br>
-              {/* <br /> */}
-              Installed Services:<br></br>
-              <h1 className='hr-purple2'></h1> 
-                Threat Intelligence Insights<br></br>
-                Data Explorer<br></br>
-                SOAR<br></br>
-                Risk Manager<br></br>
-                Guardium Insights S-Tap<br></br>
-              <br></br>
-              Looking for services to be added?
+              <div className="hr-black"></div>
+              <center>Looking for services to be added?</center>
               
-              <br></br>
-           
+            
+              
             </div>
             <div className="actions">
               <button
@@ -90,24 +117,53 @@ const ClustersInformationSecondRow = () => {
             <div className="header modalText2"> Guardium Insights </div>
             
             <div className="content modalText">
-             Guardium Insights is running on OpenShift 4.6 <br>
-            </br><br></br>
+            <div className='row mt-5 hero-row-reverse-mobile'>
+          <div className='col-lg-6 txt-md-left'>
+  
+            <h3>[Installer]</h3>
+             <h4>Douglas Freeby</h4>
+             <b>ddfreeby@us.ibm.com</b><br></br><br></br>
+             <h4>Robert Pugh</h4>
+             <b>repugh@us.ibm.com</b>
+          </div>
+          <div className='mt-3'>
+            <div className='hero-img-right'>
+            <img
+                src={douglas}
+                className=''
+                alt='Sitting on questionnaire'
+              />
+              &nbsp;&nbsp;&nbsp;
+              <img
+                src={robert}
+                className=''
+                alt='Sitting on questionnaire'
+              />
+            </div>
+          </div>
+          </div>
+
+          <div className="hr-black"></div>
+            {/* <div className="content font-weight-bold"> */}
+               
+              <h3>[Installed Services]</h3>
+             
+                &nbsp;● Guardium S-Tap<br></br>
+                &nbsp;● TBD<br></br>
+                &nbsp;● TBD<br></br>
+                <div className="hr-black"></div>  
+                <h3>[Versions]</h3>
+             
+                &nbsp;● Lopsum ipsum <br></br>
+                &nbsp;● Lopsum ipsum <br></br>
+                
+                <div className="hr-black"></div>
+                <h4>Guardium Insights is running on OpenShift 4.6</h4> 
+             <br></br>
               [ABOUT]
               <br /><br></br>
-              Installer: 
-              <h1 className='hr-purple2'></h1> 
-              Douglas Freeby + Robert Pugh<br></br><br></br>
-              {/* <br /> */}
-              Installed Services:<br></br>
-              <h1 className='hr-purple2'></h1> 
-                Guardium S-Tap<br></br>
-                TBD<br></br>
-                TBD<br></br>
-                TBD<br></br>
-              <br></br>
-              Looking for services to be added?
-             
-              <br></br>
+              <div className="hr-black"></div>
+              <center>Looking for services to be added?</center>
            
             </div>
             <div className="actions">
