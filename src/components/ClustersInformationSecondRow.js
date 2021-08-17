9ad5a9246ffe46c2499f726edc391907a915e093
cwcoogan <chasecoogan12@gmail.com>
2021-08-17 12:25:47 -0400

    cp4s installations page

--- src/components/ClustersInformationSecondRow.js
+++ src/components/ClustersInformationSecondRow.js
@@ -32,7 +32,10 @@ const ClustersInformationSecondRow = () => {
   
             <h3>[Installer]</h3>
              <h4>Jonathan Tomasulo</h4>
-             <b>jptomasu@us.ibm.com</b>
+             <h5>Client Technical Professional</h5><br></br>
+             <h4>[Contact]</h4>
+             <b>Email: jptomasu@us.ibm.com</b><br></br>
+             <b>Phone: xxx-xxx-xxx</b>
           </div>
           <div className=''>
             <div className='hero-img-right'>
@@ -46,26 +49,32 @@ const ClustersInformationSecondRow = () => {
           </div>
 
           <div className="hr-black"></div>
+                <h4>Cloud Pak 4 Security is running on OpenShift 4.6</h4> 
+             <br></br>
+             <b>Cloud Pak for Security:</b> Helps accelerate security from Threat Management to IAM. CP4S allows users to connect to their SIEM, Ticketing, or logging systems to find threats in real-time, and execute them through the SOAR platform. With automation embedded, CP4S is helping customers quickly execute and prevent threats in real-time.
+              <br /><br></br>
+
+          <div className="hr-black"></div>
             {/* <div className="content font-weight-bold"> */}
                
-              <h3>[Installed Services]</h3>
-             
-                &nbsp;● Threat Intelligence Insights <br></br>
-                &nbsp;● Data Explorer<br></br>
-                &nbsp;● SOAR<br></br>
-                &nbsp;● Risk Manager<br></br>
-                &nbsp;● Guardium Insights S-Tap<br></br>
+              <h3>[Installed Applications]</h3>
+
+                &nbsp;● <b>Data Explorer-</b> Search and analyze all of your data from one unified UI<br></br>
+                &nbsp;● <b>Threat Intelligence Insights-</b> Identify your most impactful threat with relevant threat intelligence <br></br>
+                &nbsp;● <b>Orchestration & Automation-</b> Increase cyber resilience and incident response speed<br></br>
+                &nbsp;● <b>User Behavior Analytics-</b> Detect and analyze insider threat<br></br>
+                &nbsp;● <b>Qradar Proxy-</b> connect to a Qradar deployment to allow IBM CP4S to connect to Qradar apps and API from that deployment <br></br>
+                <br></br><h3>[Beta Applications]</h3><br></br>
+                &nbsp;● <b>Risk Manager-</b> Visualize correlated security risks across domains <br></br>
+                &nbsp;● <b>Threat Investigiator-</b> automatically investigate and prioritize cases<br></br>
+
                 <div className="hr-black"></div>  
                 <h3>[Versions]</h3>
              
-                &nbsp;● Lopsum ipsum <br></br>
-                &nbsp;● Lopsum ipsum <br></br>
+                &nbsp;● <b>Cloud Pak for Security-</b> Version 1.5.0 <br></br>
+                &nbsp;● <b>OpenShift Container Platform-</b> Version 4.6 <br></br>
                 
-                <div className="hr-black"></div>
-                <h4>Cloud Pak 4 Security is running on OpenShift 4.6</h4> 
-             <br></br>
-              [ABOUT]
-              <br /><br></br>
+            
               <div className="hr-black"></div>
               <center>Looking for services to be added?</center>
               
