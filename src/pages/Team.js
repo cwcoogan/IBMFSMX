8ac9cd64f74c5f6ee10df7faa909d60ec5fcdabb
bkaimal99 <bharath.kaimal@gmail.com>
2021-08-17 15:42:48 -0500

    link team members to blue pages and linkedin

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -29,19 +29,22 @@ const Team = () => {
             <div className='text'>Cloud Pak Technical Leader</div>
           </div>
         </a>
+
+        <a href="https://w3.ibm.com/bluepages/profile.html?uid=1J4595897" target="_blank">
           <div className='image-container'>
           <div className='image john'/>
             <div className='title'>John Lucas</div>
             <div className='text'>Cloud Pak Technical Leader</div>
-
           </div>
+        </a>
 
+        <a href="https://w3.ibm.com/bluepages/profile.html?uid=4G2681897" target="_blank">
           <div className='image-container'>
           <div className='image matt'/>
             <div className='title'>Matt Konwiser</div>
             <div className='text'>Technical Sales Manager</div>
-
           </div>
+        </a>
         </div>
 
         <div className='row mt-5 hero-row-reverse-mobile home-row '>
@@ -60,38 +63,42 @@ const Team = () => {
 
         {/* environment configuration*/}
         <div className='team-row-container'>
+        <a href="https://w3.ibm.com/bluepages/profile.html?uid=535059897" target="_blank">
           <div className='image-container'>
           <div className='image duane'/>
             <div className='title'>Duane Almeter</div>
             <div className='text'>Technical Sales Leader</div>
-
           </div>
+        </a>
 
+        <a href="https://w3.ibm.com/bluepages/profile.html?uid=C-KV8R897" target="_blank">
           <div className='image-container'>
           <div className='image mitchell'/>
             <div className='title'>Mitchell Odum</div>
             <div className='text'>Technical Architect</div>
-
           </div>
+        </a>
 
+        <a href="https://w3.ibm.com/bluepages/profile.html?uid=C-R929897" target="_blank">
           <div className='image-container'>
           <div className='image mo'/>
             <div className='title'>Mohammed Alvi</div>
             <div className='text'>System Administrator</div>
           
           </div>
+        </a>
         </div>
         
+        <a href="https://w3.ibm.com/bluepages/profile.html?uid=211325897" target="_blank">
         <div className='team-row-container'>
           <div className='image-container'>
           <div className='image rich'/>
             <div className='title'>Rich Russo</div>
             <div className='text'>Chief Architect</div>
-          
           </div>
-
           
         </div>
+        </a>
 
         <div className='row mt-5 hero-row-reverse-mobile home-row '>
           <div className='col-lg-12 txt-md-left'>
@@ -109,35 +116,42 @@ const Team = () => {
 
         {/* technical sales managers*/}
         <div className='team-row-container'>
+        <a href="https://w3.ibm.com/bluepages/profile.html?uid=0G1889897" target="_blank">
           <div className='image-container'>
           <div className='image kevin'/>
             <div className='title'>Kevin Rupp</div>
             <div className='text'>Solution Engineer Manager</div>
-          
           </div>
+        </a>
 
+        <a href="https://w3.ibm.com/bluepages/profile.html?uid=5G1010897" target="_blank">
           <div className='image-container'>
           <div className='image adam-j'/>
             <div className='title'>Adam J Rodgers</div>
             <div className='text'>Technical Sales Manager</div>
-          
           </div>
+        </a>
 
+        <a href="https://w3.ibm.com/bluepages/profile.html?uid=4G6245897" target="_blank">
           <div className='image-container'>
           <div className='image steve'/>
-            <div className='title'>Steve Farque</div>
+            <div className='title'>Steve Faruque</div>
             <div className='text'>Technical Sales Manager</div>
           
           </div>
+        </a>
         </div>  
+        
 
         <div className='team-row-container'>
+        <a href="https://w3.ibm.com/bluepages/profile.html?uid=7D9055897" target="_blank">
           <div className='image-container'>
           <div className='image david-w'/>
             <div className='title'>David Waren</div>
             <div className='text'>Technical Sales Manager</div>
           
           </div>
+        </a>
         </div>
 
         
@@ -157,73 +171,81 @@ const Team = () => {
 
         {/* architect/installers*/}
         <div className='team-row-container'>
+        <a href="https://w3.ibm.com/bluepages/profile.html?uid=5D6420897" target="_blank">
           <div className='image-container'>
           <div className='image chrisc'/>
             <div className='title'>Chris Cardran</div>
             <div className='text'>Cybersecurity Architect</div>
-          
           </div>
+        </a>
 
           <div className='image-container'>
           <div className='image chrisl'/>
             <div className='title'>Christopher Liccardi</div>
             <div className='text'>Cybersecurity Architect</div>
-          
           </div>
 
+        <a href="https://w3.ibm.com/bluepages/profile.html?uid=0G2041897" target="_blank">
           <div className='image-container'>
           <div className='image chriss'/>
             <div className='title'>Christopher Suman</div>
             <div className='text'>Technical Sales</div>
-          
           </div>
+        </a>
         </div>
 
           {/* second row*/}
           <div className='team-row-container'>
+          <a href="https://w3.ibm.com/bluepages/profile.html?uid=5G2755897" target="_blank">
           <div className='image-container'>
           <div className='image david'/>
             <div className='title'>David Gibson</div>
             <div className='text'>Executive Advisor Cybersecurity</div>
-          
           </div>
+          </a>
 
+          <a href="https://w3.ibm.com/bluepages/profile.html?uid=2G1093897" target="_blank">
           <div className='image-container'>
           <div className='image douglas'/>
             <div className='title'>Douglas Freeby</div>
             <div className='text'>Cybersecurity Specialist</div>
-          
           </div>
+          </a>
 
+          <a href="https://w3.ibm.com/bluepages/profile.html?uid=180134897" target="_blank">
           <div className='image-container'>
           <div className='image jerry'/>
             <div className='title'>Jerry Steward</div>
             <div className='text'>Solutions Engineer</div>
-          
           </div>
+          </a>
         </div>
+
           {/* third row*/}
           <div className='team-row-container'>
+          <a href="https://w3.ibm.com/bluepages/profile.html?uid=4G0035897" target="_blank">
           <div className='image-container'>
           <div className='image jonathan'/>
             <div className='title'>Jonathan Tomasulo</div>
             <div className='text'>Cybersecurity Technical Specialist</div>
-          
           </div>
+          </a>
 
+          <a href="https://w3.ibm.com/bluepages/profile.html?uid=5A8148897" target="_blank">
           <div className='image-container'>
           <div className='image rajesh'/>
             <div className='title'>Rajesh Kartha</div>
             <div className='text'>Big Data Enablement</div>
-          
           </div>
+          </a>
 
+          <a href="https://w3.ibm.com/bluepages/profile.html?uid=4D0916897" target="_blank">
           <div className='image-container'>
           <div className='image robert'/>
             <div className='title'>Robert Pugh</div>
             <div className='text'>Cybersecurity Specialist</div>
-          
           </div>
+          </a>
         </div>
 
         <div className='row mt-5 hero-row-reverse-mobile home-row '>
@@ -242,43 +264,45 @@ const Team = () => {
 
         {/* interns/webdev team*/}
         <div className='team-row-container'>
+        <a href="https://www.linkedin.com/in/egomez8/" target="_blank">
           <div className='image-container'>
           <div className='image eduardo'/>
             <div className='title'>Eduardo Gomez</div>
             <div className='text'>Technical Solutions Specialist Intern</div>
-          
           </div>
+        </a>
 
+        <a href="https://www.linkedin.com/in/emily-placet/" target="_blank">
           <div className='image-container'>
           <div className='image emily'/>
             <div className='title'>Emily Placet</div>
             <div className='text'>Technical Solutions Specialist Intern</div>
-
           </div>
+        </a>
 
+        <a href="https://www.linkedin.com/in/alejandroobrador/" target="_blank">
           <div className='image-container'>
           <div className='image alejandro'/>
             <div className='title'>Alejandro Obrador</div>
             <div className='text'>Technical Solutions Specialist Intern</div>
-
           </div>
+        </a>
         </div>
         
         <div className='team-row-container'>
+        <a href="https://www.linkedin.com/in/bharath-kaimal/" target="_blank">
           <div className='image-container'>
           <div className='image bharath'/>
             <div className='title'>Bharath Kaimal</div>
             <div className='text'>Technical Solutions Specialist Intern</div>
-
           </div>
-
-          
+        </a>
         </div>
 
       </div>
       
     </div>
-
+    
   );
 };
 
